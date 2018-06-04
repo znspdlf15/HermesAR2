538f1d3d370f449deeca03bef7d840d2cd2f9d08
gitdog01 <hostdog01@naver.com>
2018-06-04 18:15:39 +0900

    firebase database 추가

--- app/src/main/java/hermes/mju/captdesign/hermesar/Mainitem/FavoriteActivity.java
+++ app/src/main/java/hermes/mju/captdesign/hermesar/Mainitem/FavoriteActivity.java
@@ -1,5 +1,6 @@
 package hermes.mju.captdesign.hermesar.Mainitem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,14 @@ public class FavoriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+
+        //databaseReference.child("chat").child(CHAT_NAME).push().setValue(chat); // 데이터 푸쉬
+       // databaseReference.child("event").child(CHAT_NAME).push().setValue(event); // 데이터 푸쉬
+
+       // Intent intent = new Intent(this, ChatRoomActivity.class);
+       // intent.putExtra("chatName", CHAT_NAME);
+       // intent.putExtra("userName", USER_NAME);
+       // startActivity(intent);
+
     }
 }
