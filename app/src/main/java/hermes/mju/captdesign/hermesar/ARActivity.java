6d9fb97a315103b940fd184eea27bad3537ed60a
Yongtak <lyt0915@naver.com>
2018-06-06 18:13:37 +0900

    6:13

--- app/src/main/java/hermes/mju/captdesign/hermesar/ARActivity.java
+++ app/src/main/java/hermes/mju/captdesign/hermesar/ARActivity.java
@@ -652,12 +652,17 @@ public class ARActivity extends AppCompatActivity implements SensorEventListener
 
     int nowPoint = 0;
     public double getNextDx(int i){
-        return (listOfPoint.get(i+1).getLocation().getLongitude() - listOfPoint.get(i).getLocation().getLongitude());
+        if ( i < listOfPoint.size() - 1 ) {
+            return (listOfPoint.get(i + 1).getLocation().getLongitude() - listOfPoint.get(i).getLocation().getLongitude());
+        }
+        return 0;
     }
     public double getNextDy(int i){
-        return (listOfPoint.get(i+1).getLocation().getLatitude() - listOfPoint.get(i).getLocation().getLatitude());
+        if ( i < listOfPoint.size() - 1 ) {
+            return (listOfPoint.get(i + 1).getLocation().getLatitude() - listOfPoint.get(i).getLocation().getLatitude());
+        }
+        return 0;
     }
-
     public void makeAnchor(Frame frame) {
         try {
             /*if (anchors.size() >= 1) {
@@ -666,28 +671,37 @@ public class ARActivity extends AppCompatActivity implements SensorEventListener
             } else {
                 anchors.add(session.createAnchor(frame.getCamera().getPose().compose(Pose.makeTranslation(0, 0, -1f).extractTranslation())));
             }*/
-            if (listOfPoint.isEmpty() != true && session != null) {
-                //while ( int i = 0; i < )
-                if (anchors.size() < 10) {
-                    float[] vector = new float[4];
-                    float dx = (float) getNextDx(0);
-                    float dy = (float) getNextDy(0);
-                    float distance = (float) Math.sqrt(dx * dx + dy * dy);
+            for (int j=0; j < listOfPoint.size(); j++){
+                if (listOfPoint.isEmpty() != true && session != null) {
+                    //while ( int i = 0; i < )
+                    if (anchors.size() < 30) {
+                        float[] vector = new float[4];
+                        float dx = (float) getNextDx(j);
+                        float dy = (float) getNextDy(j);
+                        float distance = (float) Math.sqrt(dx * dx + dy * dy);
 
-                    float deg = (float) Math.toDegrees(Math.atan(dx / dy));
-                    Quaternion target = EulerToQuat(-90, -deg, 0);        // 화살표 회전. roll (-90) : 눕히기, pitch: 방향, yaw : 안건들어도 됨
+                        float deg = (float) Math.toDegrees(Math.atan(dx / dy));
+                        if ( dy < 0 ){
+                            deg = deg + 180;
+                        }
+                        Quaternion target = EulerToQuat(-90, -deg, 0);        // 화살표 회전. roll (-90) : 눕히기, pitch: 방향, yaw : 안건들어도 됨
 
-                    vector[0] = target.x;
-                    vector[1] = target.y;
-                    vector[2] = target.z;
-                    vector[3] = target.w;
+                        vector[0] = target.x;
+                        vector[1] = target.y;
+                        vector[2] = target.z;
+                        vector[3] = target.w;
 
-                    float ddx = dx / distance;
-                    float ddy = dy / distance;
-                    for (int i = 0; i < getNextDistance(0); i++) {
-                        Pose pose = new Pose(frame.getCamera().getPose().makeTranslation(0 + i * ddx, -1f, -2f - i * ddy).getTranslation(), vector);
-                        Anchor anchor = session.createAnchor(pose);
-                        anchors.add(anchor);
+                        float ddx = dx / distance;
+                        float ddy = dy / distance;
+                        float lastX = 0;
+                        float lastY = 0;
+                        for (int i = 0; i < getNextDistance(j); i++) {
+                            Pose pose = new Pose(frame.getCamera().getPose().makeTranslation(0 + i * ddx, -1f, -2f - i * ddy).getTranslation(), vector);
+                            Anchor anchor = session.createAnchor(pose);
+                            anchors.add(anchor);
+                            lastX = 0 + i * ddx;
+                            lastY = -2f - i * ddy;
+                        }
                     }
                 }
             }
@@ -697,7 +711,7 @@ public class ARActivity extends AppCompatActivity implements SensorEventListener
             Log.e(TAG, "Exception on the OpenGL thread", t);
         }
     }
-    
+
     @Override
     public void onDrawFrame(GL10 gl10) {
 // Clear screen to notify driver it should not load any pixels from previous frame.
